044965e8a61233f56f0ea08bbb9f14659797d919
antwonlee <antwonlee@gmail.com>
2016-11-24 12:21:43 -0600

    Add pattern matching example

--- dailydrips/functions.exs
+++ dailydrips/functions.exs
@@ -3,3 +3,11 @@ print_name = fn
 end
 
 print_name.({:person, "Anthony", "Lee"})
+
+calculate_bill = fn
+  [{:item, price}, {:item, price2}] -> price + price2
+  {:item, price} -> price
+end
+
+calculate_bill.([{:item, 20}, {:item, 10}])
+calculate_bill.({:item, 35})
