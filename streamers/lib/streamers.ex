a30acdcd755fc6d993092c9ccddc316d6296d366
Anthony Lee <antwonlee@gmail.com>
2016-11-26 01:01:14 -0600

    Delete streamers

--- streamers/lib/streamers.ex
+++
@@ -1,2 +0,0 @@
-defmodule Streamers do
-end
--- streamers/test/streamers_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule StreamersTest do
-  use ExUnit.Case
-
-  test "find index file in a directory" do
-    assert Streamers.find_index("test/fixtures/emberjs") ==
-      "91lkajdfaosifj32ojrakjflkj32j23fjlkj.m3u8"
-  end
-end
