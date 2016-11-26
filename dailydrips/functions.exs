bd9365ef9490b2f252799e839c9a503ac1cb2c36
Anthony Lee <antwonlee@gmail.com>
2016-11-26 00:53:26 -0600

    Add function samples

--- dailydrips/functions.exs
+++ dailydrips/functions.exs
@@ -11,3 +11,20 @@ end
 
 calculate_bill.([{:item, 20}, {:item, 10}])
 calculate_bill.({:item, 35})
+
+greet_person = fn
+  greeter, {:person, first_name, last_name} -> greeter.(first_name <> " " <> last_name)
+end
+
+polite_greeter = fn
+  name -> "Hello, #{name}, nice to meet you!"
+end
+
+terse_greeter = fn
+  name -> "Hi #{name}"
+end
+
+person = {:person, "Josh", "Adams"}
+
+greet_person.(polite_greeter, person)
+greet_person.(terse_greeter, person)
