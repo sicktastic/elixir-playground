86dcaac33a4785bcd05f10c2208c0c923c823632
Anthony Lee <antwonlee@gmail.com>
2016-12-15 00:02:08 -0600

    Add nested modules

--- basic/functions_modules_structs.exs
+++ basic/functions_modules_structs.exs
@@ -8,13 +8,34 @@
 
 # named function
 
-def add(a,b) do
-  a + b
-end
+# def add(a,b) do
+#   a + b
+# end
 
 # named functions can only be defined in modules:
+# defmodule Math do
+#   def add42(a, b) do
+#     add(a,b) + 42
+#   end
+
+#   def add(a, b) do
+#     add(a,b)
+#   end
+# end
+
+# how to call a named function:
+
+# Math.add(1, 2)
+
+# modules can be nested:
+
 defmodule Math do
-  def add(a, b) do
-    add(a,b)
+  defmodule Division do
+    def divide(a, b) do
+      a / b
+    end
   end
 end
+
+IO.puts Math.Division.divide(15, 5)
+
