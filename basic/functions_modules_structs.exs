c5350d7311e61880aea32eaba39dd51a35597841
Anthony Lee <antwonlee@gmail.com>
2017-01-08 23:26:21 -0600

    Add documentation examples

--- basic/functions_modules_structs.exs
+++ basic/functions_modules_structs.exs
@@ -1,75 +1,47 @@
-# add = fn(a, b) ->
-#   a + b
-# end
-
-# hello = add
-# add.(1, 2)
-# hello.(1 ,2)
-
-# named function
-
-# def add(a,b) do
-#   a + b
-# end
-
-# named functions can only be defined in modules:
-# defmodule Math do
-#   def add42(a, b) do
-#     add(a,b) + 42
-#   end
-
-#   def add(a, b) do
-#     add(a,b)
-#   end
-# end
-
-# how to call a named function:
-
-# Math.add(1, 2)
-
-# modules can be nested:
-
-# defmodule Math do
-#   defmodule Division do
-#     def divide(a, b) do
-#       a / b
-#     end
-#   end
-# end
-
-# IO.puts Math.Division.divide(100, 2)
-
-# def add(a, b) do
-#   a + b
-# end
-
-# defmodule User do
-#   defstruct name: nil, email: nil
-# end
-
-# %User{}
-# %User{name: "Anthony"} # => {__struct__: User, name: "Anthony", email: nil}
-
-# # Structs: Under the hood
-
-# defmodule User do
-#   def __struct__ do
-#     %{__struct__: User, name: "Anthony", email: nil}
-#   end
-# end
-
 defmodule User do
+  @moduledoc """
+  Defines the user struct and functions to handle users.
+  """
+  import String, only: [split: 1]
+  import List, only: [first: 1, last: 1]
+
   defstruct name: nil, email: nil
 
+  @doc """
+  Get the first name of a user.
+
+  ## Parameters
+  
+  - `user` - A user struct.
+
+  ## Examples
+
+    user = %User{name: "Anthony Lee"}
+    User.first_name(user)
+    "Anthony"
+  """
   def first_name(user) do
     user.name
-    |> String.split
-    |> List.first
+    |> split
+    |> first
   end
 
+  @doc """
+  Get the last name of a user.
+
+  ## Parameters
+  
+  - `user` - A user struct.
+
+  ## Examples
+
+    user = %User{name: "Anthony Lee"}
+    User.last_name(user)
+    "Lee"
+  """
   def last_name(user) do
     user.name
-    |> String.split
-    |> List.last
+    |> split
+    |> last
   end
 end
