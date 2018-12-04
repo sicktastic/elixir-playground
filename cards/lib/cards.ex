d3f83d859153d1f4ed6e5fae9d08b2aea2a5d473
Anthony Lee <antwonlee@gmail.com>
2018-12-04 11:31:33 -0600

    Add shuffle
    https://goo.gl/wrQSRZ

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,10 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
-  def hello do
-    :world
+  # https://goo.gl/wrQSRZ
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
 end
