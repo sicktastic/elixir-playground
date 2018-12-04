707c47da2095e3c5d7ded2f60d0e9ec706e20e81
Anthony Lee <antwonlee@gmail.com>
2018-12-04 11:55:24 -0600

    Add nested arrays

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,10 +1,51 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = [
+              "Ace", 
+              "Two", 
+              "Three", 
+              "Four", 
+              "Five", 
+              "Six", 
+              "Seven", 
+              "Eight", 
+              "Nine", 
+              "Ten", 
+              "Jack", 
+              "Queen", 
+              "King"
+            ]
+
+    suits = ["Spades", "Club", "Hearts", "Diamonds"]
+
+    # https://goo.gl/SZM8FT
+    # bad approach
+    # for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
+
+    for suit <- suits,  value <- values do
+      "#{value} of #{suit}"
+    end
+
   end
 
   # https://goo.gl/wrQSRZ
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  # https://goo.gl/wXxD2Z
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
